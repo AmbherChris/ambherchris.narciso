187b7403bb8212f22889befee39a6c9c15733c6e
AmbherChris <nambher@gmail.com>
2025-05-14 19:50:05 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,73 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex flex-col bg-amber-200 justify-center items-center ">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+      <div className="relative mt-20 flex flex-col bg-amber-800 items-center text-center ">
+        <h1 className="font-onest text-7xl  transform ">
+          AMB&nbsp; ER
+        </h1>
+
+        <h1 className="text-7xl font-regular transform scale-y-205 left-49.5 absolute  top-[28px] ">
+          H
+        </h1>
+        <h1 className="text-7xl font-regular transform  left-0.5 absolute w-lg   top-[55px] ">
+          C&nbsp; RIS
+        </h1>
+        <div className="flex justify-center items-center gap-4 mt-24 ">
+          <Image
+            src="/fb.png"       // Place the image inside /public folder
+            alt="Description"
+            width={512}
+            height={512}
+            className="h-10 w-auto mr-2"
+          />
+
+          <Image
+            src="/linkedin.png"       // Place the image inside /public folder
+            alt="Description"
+            width={512}
+            height={512}
+            className="h-11 w-auto mb-[2px]"
+          />
+          <Image
+            src="/mail.png"       // Place the image inside /public folder
+            alt="Description"
+            width={512}
+            height={512}
+            className="h-16 w-auto"
+          />
+          <Image
+            src="/github.png"       // Place the image inside /public folder
+            alt="Description"
+            width={512}
+            height={512}
+            className="h-11 w-auto"
+          />
+
+          
+
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <p className="text-center text-[14px]">
+            I'm a creative individual with a keen interest in designing, passionate about creating clean and user-friendly interfaces with a strong focus on UX in the tech field. I'm always eager to learn and grow as a designer and developer.
+          </p>
+      </div>
+
+
+      <h1 className="mt-100">About Me</h1>
+
+
+      <div className="bg-amber-400 size-7 mt-100">
+
+      </div>
+
+
+
+
     </div>
   );
 }
